c794ddcedba3c39cfae71fa4541c2047b3189f1b
Andreas-Nidis <a_kalketenidis@hotmail.com>
2025-01-07 14:48:23 +0100

    Setup main background image and title. Also setup skeleton for Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Header= (props) => {
+    return (
+        <Nav>
+            <Logo>
+                <img src="" alt="" />
+            </Logo>
+
+            <NavMenu>
+                <a>
+                    <span>Home</span>
+                </a>
+                <a>
+                    <span>About</span>
+                </a>
+                <a>
+                    <span>Request</span>
+                </a>
+            </NavMenu>
+        </Nav>
+    )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,46 @@ const Home = (props) => {
             <Title>
                 <h1>WoodIt</h1>
             </Title>
-            <BackgroundImg>
-                <img src="../images/main-background-image.jpg" alt="WoodIt Background Image"/>
-            </BackgroundImg>
         </Container>
     )
 };
 
 const Container = styled.main`
     position: relative;
+    display: block;
+    margin: 0;
+    padding: 0;
+    top: 0;
 `;
 
 const Title = styled.div`
     position: relative;
-`;
+    color: #eeeeee;
+    margin: 0;
+    padding: 0;
+    top: 0;
+    min-height: 400px;
+    min-width: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-const BackgroundImg = styled.img`
-    position: relative;
+    h1 {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        font-size: 50px;
+    }
+
+    &:after {
+        background: url('/images/main-background-1.jpg') center center / cover no-repeat fixed;
+        content: '';
+        position: absolute;
+        inset: 0px;
+        opacity: 0.98;
+        z-index: -1;
+    }
 `;
 
 export default Home;
